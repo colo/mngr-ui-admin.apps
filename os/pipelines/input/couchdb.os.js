911796cd58ad83d99e9319da5b4bd1fd4ed76e1c
Luciano Bueno <glamero@gmail.com>
2018-10-02 18:37:07 -0300

    bump

--- os/pipelines/input/couchdb.os.js
+++ os/pipelines/input/couchdb.os.js
@@ -11,6 +11,10 @@ module.exports = new Class({
 
 
   options: {
+    couchdb: {
+      request: require('cachemachine')({redis: true, hostname: 'elk'})
+    },
+
     paths: [],
     stat_host: null,
     range_path: undefined,
@@ -139,41 +143,41 @@ module.exports = new Class({
 			],
 			periodical: [
         /**
-        * peridically exec this view to keep it warm on couchdb
+        * peridically exec this view to keep it warm on couchdb/redis
         **/
-        // {
-				// 	sort_by_path: function(req, next, app){
-        //     //console.log('SORT_BY_PATH RANGE', app.options.stat_host, req)
-        //     let path = 'os'
-        //     // let range = req.opt.range
-        //
-        //     if(app.options.stat_host){
-        //       let end = Date.now()
-        //       let start = end - 1000
-        //
-        //               // next(
-        //               app.view({
-        //   							uri: app.options.db,
-        //                 args: [
-        //                   'sort',
-        //                   'by_path',
-        //                   {
-        //                     startkey: [path, app.options.stat_host, "periodical", start],
-        //     								endkey: [path, app.options.stat_host, "periodical",end],
-        //
-        //     								inclusive_end: true,
-        //     								include_docs: false
-        //     							}
-        //                 ]
-        //   						})
-        //
-        //
-        //     }
-        //
-        //
-        //
-				// 	}
-				// },
+        {
+					sort_by_path: function(req, next, app){
+            console.log('WARM SORT_BY_PATH RANGE', app.options.stat_host)
+            let path = 'os'
+            // let range = req.opt.range
+
+            if(app.options.stat_host){
+              // let end = Date.now()
+              // let start = end - 1000
+
+                      // next(
+                      app.view({
+          							uri: app.options.db,
+                        args: [
+                          'sort',
+                          'by_path',
+                          {
+                            startkey: [path, app.options.stat_host, "periodical"],
+            								endkey: [path, app.options.stat_host, "periodical\ufff0"],
+
+            								inclusive_end: true,
+            								include_docs: false
+            							}
+                        ]
+          						})
+
+
+            }
+
+
+
+					}
+				},
 
         {
 					sort_by_host: function(req, next, app){
